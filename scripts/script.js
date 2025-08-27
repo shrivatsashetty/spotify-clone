57ae20e1126007abb9ac61b4ccaf695486080bb8
shrivatsashetty <shrivatsa307@gmail.com>
2025-08-27 23:25:35 +0530

    added songs card in the sidebar

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,16 @@
 
-async function fetchSongs() {
+/* global variables */
+let isSongPlaying = false;
+
+let arrSongPaths = [];
+
+let currentSong = new Audio();
+
+let currentSongIndex = 0
+
+const btnPlayPauseSong = document.getElementById("btn-play-pause-song");
+
+async function fetchSongPaths() {
     let arrSongPaths = [];
 
     let response = await fetch("http://127.0.0.1:3000/files/song-files/");  
@@ -19,8 +30,52 @@ async function fetchSongs() {
         arrSongPaths.push(element.href);
     }
 
-    console.log(arrSongPaths);
-    
+    /* this function returns a promise
+     * this promise resolves if the array of song paths is not null
+     * otherwise the promise is rejected */
+    return new Promise(
+        (resolve, reject) => {
+            if(arrSongPaths) {
+                resolve(arrSongPaths);
+            }
+            else {
+                reject("Error, No Songs Found!!");
+            }
+        }
+    );
+
 }
 
-fetchSongs();
+document.addEventListener(
+    "DOMContentLoaded",
+    async () => {
+       arrSongPaths = await fetchSongPaths();
+       currentSong.src = arrSongPaths[0];
+    }
+);
+
+currentSong.addEventListener(
+    "ended",
+    () => {
+        if(currentSongIndex == arrSongPaths.length - 1) {
+            currentSongIndex = 0;
+        }
+
+        currentSongIndex = currentSongIndex += 1;
+    }
+);
+
+btnPlayPauseSong.addEventListener(
+        "click",
+        async () => {
+            if (currentSong.paused) {
+                console.log();
+                currentSong.play();
+                btnPlayPauseSong.firstElementChild.src = "assets/icons/pause.svg"
+            }
+            else {
+                currentSong.pause();
+                btnPlayPauseSong.firstElementChild.src = "assets/icons/play.svg"
+            }
+        }
+    );
