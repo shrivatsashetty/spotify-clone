b4ca597639cbe811421a13b47d01215530cf6406
shrivatsashetty <shrivatsa307@gmail.com>
2025-08-31 22:50:57 +0530

    added songname and elapsed time labels to the media player

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 
 /* global variables */
+const domParser = new DOMParser();
 
 let arrSongPaths = [];
 
 let arrSongObjs = [];
 
 let currentSong = new Audio();
+
+let currentSongObj;
 
 let indexCurrentSong = 0;
 
@@ -14,7 +17,7 @@ const btnPlayPauseSong = document.getElementById("btn-play-pause-song");
 const btnPlayPreviousSong = document.getElementById("btn-play-previous-song");
 const btnPlayNextSong = document.getElementById("btn-play-next-song");
 
-const containerSongCards = document.querySelector(".container-song-cards");
+const containerSongCards = document.querySelector("ul.container-song-cards");
 
 
 /* an asynchronous function that fetches the resources in the song file directory 
@@ -26,7 +29,6 @@ async function fetchSongPaths() {
     let strSongsDir = await response.text();
 
     /* parsing the html formatted string to an HTML document */ 
-    const domParser = new DOMParser();
     const docSongsDir = domParser.parseFromString(strSongsDir, 'text/html');
 
     /* the songs will be stored in the td>a element of the doucument */
@@ -37,9 +39,7 @@ async function fetchSongPaths() {
     for (let i = 1; i < songPaths.length; i++) {
         const element = songPaths[i];
         arrSongPaths.push(element.href);
-    }
-    console.log(arrSongPaths);
-    
+    }    
 
     /* this function returns a promise
      * this promise resolves if the array of song paths is not null
@@ -64,6 +64,9 @@ function getSongNameFromPath(songFilePath) {
     let songName = songFilePath.split("/").at(-1);
     /* remove the extension ".mp3" */
     songName = songName.replace(".mp3", "");
+
+    songName = songName.replaceAll("_", " ");
+
     return songName;
 }
 
@@ -88,7 +91,6 @@ function createSongObjs(arrSongPaths) {
         );
 
     }
-    console.log(arrSongObjs);
     
 }
 
@@ -97,6 +99,7 @@ function createSongObjs(arrSongPaths) {
 function playPauseCurrentSong() {
 
     highlightCurrentSong();
+    updateCurrentSongLabel();
 
     if (currentSong.paused) {
         currentSong.play();
@@ -107,6 +110,38 @@ function playPauseCurrentSong() {
         btnPlayPauseSong.firstElementChild.src = "assets/icons/play.svg"
     }
 }
+
+/* a function to play any selected song from the array of songs
+ * the same function can be reused for handling click of nextSong and previous song */
+function updateCurrentSong(songIndex) {
+    if (songIndex > arrSongPaths.length -1) {
+        indexCurrentSong = 0;
+    }
+    else if (songIndex < 0) {
+        indexCurrentSong = arrSongPaths.length - 1;
+    }
+    else {
+        indexCurrentSong = songIndex;
+    }
+
+    /* update the src attribute of the current Audio element
+     * i.e. currentSong */
+    currentSong.src = arrSongPaths[indexCurrentSong];
+
+    /* also update the current song object */
+    currentSongObj = arrSongObjs[indexCurrentSong];
+
+    playPauseCurrentSong();
+}
+
+
+/* this function updates the name of the currently playing song label
+ * in the media controls */
+function updateCurrentSongLabel() {
+    const lblCurrentSong = document.querySelector(".media-state p.media-label");
+    lblCurrentSong.innerText = currentSongObj.songName;
+}
+
 
 function highlightCurrentSong() {
     
@@ -124,32 +159,14 @@ function highlightCurrentSong() {
             .classList.add("highlighted-card");
 }
 
-/* a function to play any selected song from the array of songs
- * the same function can be reused for handling click of nextSong and previous song */
-function playSelectedSong(songIndex) {
-    if (songIndex > arrSongPaths.length -1) {
-        indexCurrentSong = 0;
-    }
-    else if (songIndex < 0) {
-        indexCurrentSong = arrSongPaths.length - 1;
-    }
-    else {
-        indexCurrentSong = songIndex;
-    }
-    currentSong.src = arrSongPaths[indexCurrentSong];
-
-    playPauseCurrentSong();
-}
-
 
 /* a function to create the songs library */
 function createSongsLibrary() {
     
     for (const songObj of arrSongObjs) {
-        let songCard = `
-            <div class="card-song flexbox space-between rounded-corners pad-1 cursor-pointer"
+        let strSongCardHTML = `
+            <li class="card-song flexbox space-between rounded-corners pad-1 cursor-pointer"
                 data-song-name="${songObj.songName}" data-song-path="${songObj.songPath}" data-song-index="${songObj.songIndex}"
-                onclick="playSelectedSong(${songObj.songIndex})"
             >
 
                 <div class="song-label flexbox">
@@ -169,14 +186,25 @@ function createSongsLibrary() {
                 <img class="invert" 
                     src="assets/icons/tripple-dots-vertical.svg" alt="Options Icon"
                 >
-            </div>
+            </li>
             `
 
-        /* updating the innerHTML of the card container by using the above crated element */
-        containerSongCards.innerHTML = containerSongCards.innerHTML + songCard;
+        /* converting the above HTML formatted string to a HTML document and then an HTML element*/        
+        let songCard = domParser.parseFromString(strSongCardHTML, "text/html");
+        songCard = songCard.querySelector("li");
+
+        /* adding an event listner to each song */
+        songCard.addEventListener(
+            "click",
+            () => {
+                updateCurrentSong(songObj.songIndex);
+            }
+        );
+
+        containerSongCards.appendChild(songCard);
+
     }    
 }
-
 
 
 document.addEventListener(
@@ -186,16 +214,21 @@ document.addEventListener(
 
         arrSongPaths = await fetchSongPaths();
 
+        /* intializing the src attribute for the currentSong
+         * at first it will be first song in the library */
         currentSong.src = arrSongPaths[0];
 
         /* intiate the creation of song object arrays */
         createSongObjs(arrSongPaths);
 
+        currentSongObj = arrSongObjs[0];
+
+
         /* after the current song finshes playing, we need to autoplay the next song */
         currentSong.addEventListener(
             "ended",
             () => {
-                playSelectedSong(indexCurrentSong + 1);
+                updateCurrentSong(indexCurrentSong + 1);
             }
         );
 
@@ -208,14 +241,14 @@ document.addEventListener(
         btnPlayNextSong.addEventListener(
             "click",
             () => {
-                playSelectedSong(indexCurrentSong + 1);
+                updateCurrentSong(indexCurrentSong + 1);
             }
         );
 
         btnPlayPreviousSong.addEventListener(
             "click",
             () => {
-                playSelectedSong(indexCurrentSong -1);
+                updateCurrentSong(indexCurrentSong -1);
             }
         );
 
